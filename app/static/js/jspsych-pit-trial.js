9bf4c38b82e80df2f1ece667510fd433197967fb
Sam Zorowitz <szorowi1@gmail.com>
2020-02-05 17:44:55 -0500

    very simple keypress task

---
+++ app/static/js/jspsych-pit-trial.js
@@ -0,0 +1,127 @@
+/**
+ * jspsych-pit-trial
+ **/
+
+
+jsPsych.plugins["pit-trial"] = (function() {
+
+  var plugin = {};
+
+  plugin.info = {
+    name: 'pit-trial',
+    description: '',
+    parameters: {
+      choices: {
+        type: jsPsych.plugins.parameterType.KEYCODE,
+        array: true,
+        pretty_name: 'Choices',
+        default: jsPsych.ALL_KEYS,
+        description: 'The keys the subject is allowed to press to respond to the stimulus.'
+      },
+      trial_duration: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Trial duration',
+        default: null,
+        description: 'How long to show trial before it ends.'
+      },
+      response_ends_trial: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Response ends trial',
+        default: true,
+        description: 'If true, trial will end when subject makes a response.'
+      },
+
+    }
+  }
+
+  plugin.trial = function(display_element, trial) {
+
+    var new_html = `
+    <div id="jspsych-pit-trial-stimulus">
+
+      <div class="wrap">
+
+        <div class="machine-back"></div>
+        <div class="conveyor"></div>
+        <div class="shadows"></div>
+        <div class="window"></div>
+        <div class="machine-front"></div>
+        <div class="machine-top"></div>
+
+      </div>
+
+    </div>`;
+
+    // draw
+    display_element.innerHTML = new_html;
+
+    // store response
+    var response = {
+      rt: null,
+      key: null
+    };
+
+    // function to end trial when it is time
+    var end_trial = function() {
+
+      // kill any remaining setTimeout handlers
+      jsPsych.pluginAPI.clearAllTimeouts();
+
+      // kill keyboard listeners
+      if (typeof keyboardListener !== 'undefined') {
+        jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
+      }
+
+      // gather the data to store for the trial
+      var trial_data = {
+        "rt": response.rt,
+        "stimulus": trial.stimulus,
+        "key_press": response.key
+      };
+
+      // clear the display
+      display_element.innerHTML = '';
+
+      // move on to the next trial
+      jsPsych.finishTrial(trial_data);
+    };
+
+    // function to handle responses by the subject
+    var after_response = function(info) {
+
+      // after a valid response, the stimulus will have the CSS class 'responded'
+      // which can be used to provide visual feedback that a response was recorded
+      display_element.querySelector('#jspsych-pit-trial-stimulus').className += ' responded';
+
+      // only record the first response
+      if (response.key == null) {
+        response = info;
+      }
+
+      if (trial.response_ends_trial) {
+        end_trial();
+      }
+    };
+
+    // start the response listener
+    if (trial.choices != jsPsych.NO_KEYS) {
+      var keyboardListener = jsPsych.pluginAPI.getKeyboardResponse({
+        callback_function: after_response,
+        valid_responses: trial.choices,
+        rt_method: 'performance',
+        persist: false,
+        allow_held_key: false
+      });
+    }
+
+    // end trial if trial_duration is set
+    if (trial.trial_duration !== null) {
+      jsPsych.pluginAPI.setTimeout(function() {
+        end_trial();
+      }, trial.trial_duration);
+    }
+
+  };
+
+  return plugin;
+})();
