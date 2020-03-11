8590450daa81e7af8451e8136e6d8c6ba1fc7f30
Sam Zorowitz <szorowi1@gmail.com>
2020-03-11 14:04:57 -0400

    second pilot

--- notebooks/03_Pilot/scripts/collate_data.py
+++ notebooks/03_Pilot/scripts/collate_data.py
@@ -29,10 +29,37 @@ for f in files:
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
     
     ## Assemble behavioral data.
-    cols = ['Block','Trial','Valence','Action','Correct','Choice','RT','Accuracy','Sham','Outcome']
     data = DataFrame([dd for dd in JSON if dd['trial_type'] == 'pit-trial'])
-    data = data[cols].query('Block > 0')
-    
+    data = data.query('Block > 0')
+
+    ## Version testing.
+    try: 
+        
+        ## Define columns of interest.
+        cols = ['Block','Trial','Valence','Action','Robot','Rune','Correct',
+                'Choice','RT','Accuracy','Sham','Outcome','TotalKeys']
+        
+        ## Limit to columns of interest.
+        data = data[cols]
+        
+        ## Insert version ID.
+        data.insert(0,'Version',2)
+        
+    except KeyError:
+                
+        ## Define columns of interest.
+        cols = ['Block','Trial','Valence','Action','Correct','Choice',
+                'RT','Accuracy','Sham','Outcome']
+        
+        ## Limit to columns of interest.
+        data = data[cols]
+        
+        ## Insert version ID and missing columns.
+        data.insert(0,'Version',1)
+        data.insert(5,'Robot',np.nan)
+        data.insert(6,'Rune',np.nan)
+        data['TotalKeys'] = np.nan
+
     ## Reformat columns.
     data['Block'] = data['Block'].astype(int)
     data['Trial'] = data['Trial'].astype(int)
@@ -42,7 +69,7 @@ for f in files:
     
     ## Insert subject ID. Append.
     data.insert(0,'Subject',subject)
-    DATA.append(data)
+    DATA.append(data)    
     
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
     ### Assemble metadata.
@@ -50,19 +77,22 @@ for f in files:
     
     ## Gather surveys.
     DEMO, = [dd for dd in JSON if dd['trial_type'] == 'survey-demo']
+    COMP, = [dd for dd in JSON if dd['trial_type'] == 'pit-comprehension']
     DEBRIEF, = [dd for dd in JSON if dd['trial_type'] == 'survey-debrief']
     
      ## Assemble dictionary.
     dd = dict(Subject = subject)
     dd.update(DEMO['demographics']); dd['Demo-RT'] = DEMO['rt'] / 1000.
+    dd['Comp-Errors'] = COMP['errors']; dd['Comp-RT'] = COMP['rt'] / 1000.
     dd.update(DEBRIEF['debriefgraphics']); dd['Debrief-RT'] = DEMO['rt'] / 1000.
     
     ## Append.
     METADATA.append(dd)
     
 ## Concatenate data.
-DATA = concat(DATA)
-METADATA = DataFrame(METADATA, columns=dd.keys())
+DATA = concat(DATA).sort_values(['Version','Subject','Trial'])
+METADATA = DataFrame(METADATA, columns=dd.keys()).sort_values('Subject')
+METADATA = METADATA.merge(DATA[['Subject','Version']])
 
 ## Save.
 DATA.to_csv(os.path.join(DATA_DIR, 'data.csv'), index=False)
