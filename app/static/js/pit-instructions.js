c8304f343b5b4c7edc472801f9e3137760418992
Sam Zorowitz <szorowi1@gmail.com>
2020-11-17 11:52:53 -0500

    pit v0.1 (11) - clean up instructions

--- app/static/js/pit-instructions.js
+++ app/static/js/pit-instructions.js
@@ -5,11 +5,11 @@
 var instructions_01 = {
   type: 'pit-instructions',
   pages: [
-    "Welcome to the<br><b>Robot Factory!</b>",
+    "Welcome to the <b>Robot Factory</b> game!",
     "In this game, you will be inspecting robots as they move down the assembly line into the <b>scanner</b>.",
     "The robots in the factory are sometimes in need of repair.",
-    "When a robot is scanned, you must decide whether to:<br>Repair the robot (press SPACE). <br>Pass the robot (do nothing).",
-    "Next we will practice these actions.<br>Four robots will come down the assembly line.<br><b>Repair</b> each robot by pressing SPACE.",
+    "When a robot is scanned, you must decide whether to:<br><b>Repair</b> the robot (press SPACE) <br><b>Ignore</b> the robot (do nothing)",
+    "Next you will practice these actions.<br>Four robots will come down the assembly line.<br><b>Repair</b> each robot by pressing SPACE.",
     "<b>HINT:</b> Only press once the robot is in the scanner<br>and the scanner light comes on."
   ],
   show_clickable_nav: true,
@@ -42,7 +42,7 @@ var practice_go = {
 var instructions_02 = {
   type: 'pit-instructions',
   pages: [
-    "Great job!<br>Four more robots will come down the assembly line.<br>Pass each robot by doing nothing.",
+    "Great job!<br>Four more robots will come down the assembly line.<br><b>Ignore</b> each robot by doing nothing.",
   ],
   show_clickable_nav: true,
   button_label_previous: "Prev",
@@ -72,7 +72,7 @@ var quiz_01 = {
   type: 'pit-comprehension',
   prompts: [
     "To <b>repair</b> a robot, what do you do?",
-    "To <b>pass</b> a robot (i.e. do not repair), what do you do?",
+    "To <b>ignore</b> a robot, what do you do?",
   ],
   options: [
     ["Press SPACE", "Do nothing", "Press ENTER"],
@@ -92,35 +92,23 @@ var instructions_03 = {
   type: 'pit-instructions',
   pages: [
     "There are different types of robots in the factory.<br>Some types of robot will need repair <b>more often</b> than others.",
-    "You can tell a robot's type by the <b>letter</b> on its<br>chestplate as revealed by the scanner.",
-    "During the game, you can win or avoid losing points by fixing robots that need pair and passing robots that do not.",
-    "Importantly, whether you can win or lose points<br>depends on whether the robot is SAFE or DANGEROUS.",
-    `If the scanner is <b><font color=${outcome_color_win}>${instr_color_win}</font></b>, the robot is SAFE. You will earn +10 points for correctly fixing or passing a safe robot. Incorrect actions will earn you +1 point.`,
-    `If the scanner is <b><font color=${outcome_color_win}>${instr_color_win}</font></b>, the robot is DANGEROUS. You will lose only -1 points for correctly fixing or passing a dangerous robot. Incorrect actions will cost you -10 points.`,
-    "Pay close attention to the robot's symbol as it will help you<br>decide whether to fix the robot (press SPACE)<br>or pass the robot (do nothing).",
-    "<b>HINT:</b> Not all robots of a type will need repair, but some<br>types of robots will need fixing more often than others.",
+    "You can tell a robot's type by the <b>symbol</b> on its<br>chestplate as revealed by the scanner.",
+    "<b>HINT:</b> Not all robots of the same type will need repair, but<br>some types of robots will need repair more often than others.",
+    "During the game, you will earn the most points by fixing robots in need of repair and ignoring robots that do not.",
+    `Importantly, how many points you can win or lose depends<br>on whether the robot is <b><font color=${outcome_color_win}>SAFE</font></b> or <b><font color=${outcome_color_lose}>DANGEROUS</font></b>.`,
+    `When the scanner is <b><font color=${outcome_color_win}>${instr_color_win}</font></b>, the robot is <b><font color=${outcome_color_win}>SAFE</font></b>.<br>You will earn +10 points for correctly repairing or ignoring<br>a safe robot. Incorrect actions will earn you +1 point.`,
+    `If the scanner is <b><font color=${outcome_color_lose}>${instr_color_lose}</font></b>, the robot is <b><font color=${outcome_color_lose}>DANGEROUS</font></b>.<br>You will lose only -1 point for correctly repairing or ignoring<br>a dangeorus robot. Incorrect actions will cost you -10 points.`,
+    "Pay close attention to the robot's symbol as it will help you<br>decide whether to repair the robot (press SPACE)<br>or ignore the robot (do nothing).",
     "You should try to earn as many points as you can, even if it's not possibe to win points or avoid losing points on every round.",
     "At the end of the task, the total number of points you've<br>earned will be converted into a <b>performance bonus.</b>",
+    "Next, we will ask you some questions about the task.<br>You need to answer all questions correctly to proceed.",
   ],
   robot_runes: [
-    '',
-    '',
-    '',
-    'a',
-    'a',
-    '',
-    '',
-    '',
+    '', 'A', 'A', '', '', 'A', 'A', 'A', '', '', ''
   ],
   scanner_colors: [
-    '#FFFFFF00',
-    scanner_color_win,
-    scanner_color_lose,
-    '#FFFFF080',
-    '#FFFFF080',
-    '#FFFFFF00',
-    '#FFFFFF00',
-    '#FFFFFF00'
+    '#FFFFFF00', '#FFFFF080', '#FFFFF080', '#FFFFFF00', '#FFFFFF00',
+    scanner_color_win, scanner_color_lose, '#FFFFF080', '#FFFFFF00', '#FFFFFF00', '#FFFFFF00'
   ],
   show_clickable_nav: true,
   button_label_previous: "Prev",
@@ -130,8 +118,8 @@ var instructions_03 = {
 var quiz_02 = {
   type: 'pit-comprehension',
   prompts: [
-    `When the scanner light is <b><font color=${outcome_color_win}>${instr_color_win}</font></b>, how many points will you earn for a correct decision?`,
-    `When the scanner light is <b><font color=${outcome_color_lose}>${instr_color_lose}</font></b>, how many points will you earn for a correct decision?`,
+    `When the scanner light is <b><font color=${outcome_color_win}>${instr_color_win}</font></b>, how many points will you earn for a correct action?`,
+    `When the scanner light is <b><font color=${outcome_color_lose}>${instr_color_lose}</font></b>, how many points will you earn for a correct action?`,
     "<i>True</i> or <i>False</i>: Some robots will need repair more often than others.",
     "<i>True</i> or <i>False</i>: The points I earn will affect my performance bonus."
   ],
